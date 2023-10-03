5d1e95aecca1dfafa9600a33f409519f5eb12c73
SaurabhMishra <saurabhrmishra3253@gnail.com>
2023-10-03 12:57:44 +0530

    DeleteExpanseItem

--- src/component/Expanse/ExpanseItem.js
+++ src/component/Expanse/ExpanseItem.js
@@ -3,17 +3,27 @@ import React from 'react';
 import ExpanseDate from './ExpanseDate';
 import './ExpanseItem.css';
 
- const ExpanseItem=(props) =>{
+const ExpanseItem = (props) => {
     
+// Delete Expanse Item
+    const handleDeleteClick = () => {
+        // Perform the deletion logic here, remove the item from the DOM
+        const itemToDelete = document.querySelector('.expanse_item');
+        if (itemToDelete) {
+            itemToDelete.remove();
+        }
+    };
+
     return (
-        <div className=" expanse_item">
+        <div className="expanse_item">
             <div>{props.expanseItem}</div>
-           <ExpanseDate date={props.date}/>
+            <ExpanseDate date={props.date} />
             <div className="expanse_item_discription">
                 <h2>{props.title}</h2>
                 <div className="expanse_item_price">{props.LocationOfExpenditure}</div>
                 <div className="expanse_item_price">Rs {props.amount}</div>
             </div>
+            <button onClick={handleDeleteClick}>Delete Expense</button>
         </div>
     );
 }
