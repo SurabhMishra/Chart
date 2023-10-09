601bad866bb05bcacf7d47f4a21646836ec0a2d6
SaurabhMishra <saurabhrmishra3253@gnail.com>
2023-10-09 13:04:53 +0530

    Rendring

--- src/App.js
+++ src/App.js
@@ -1,75 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import NewExpense from './component/NewExpense/NewExpense';
 import Expanse from './component/Expanse/Expanse';
 
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    expanseItem: 'Expanse Item',
+    title: 'Food',
+    amount: 1000,
+    date: new Date(2023, 2, 15),
+    LocationOfExpenditure: 'Location:- Restaurant Fifty Five East',
+  },
+  {
+    id: 'e2',
+    expanseItem: 'Expanse Item',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2023, 5, 12),
+    LocationOfExpenditure: 'Location:- Allahabad',
+  },
+  {
+    id: 'e3',
+    expanseItem: 'Expanse Item',
+    title: 'Toilet Paper',
+    amount: 999,
+    date: new Date(2023, 4, 4),
+    LocationOfExpenditure: 'Location:- Bhadohi',
+  },
+  {
+    id: 'e4',
+    expanseItem: 'Expanse Item',
+    title: 'Car Insurance',
+    amount: 5000,
+    date: new Date(2023, 9, 11),
+    LocationOfExpenditure: 'Location:- Suriyawan',
+  },
+  {
+    id: 'e5',
+    expanseItem: 'Expanse Item',
+    title: 'New TV',
+    amount: 4000,
+    date: new Date(2023, 6, 6),
+    LocationOfExpenditure: 'Location:- Varanasi',
+    expanseAmount: 'Expanse of Amount',
+  },
+  {
+    id: 'e6',
+    expanseItem: 'Expanse Item',
+    title: 'Movie',
+    amount: 700,
+    date: new Date(2023, 8, 15),
+    LocationOfExpenditure: 'Location:- Lucknow',
+  },
+  
+];
+
 // arrow functioms
 const App = () =>{
-
-  const expanses = [
-    {
-      id: 'e1',
-      expanseItem: 'Expanse Item',
-      title: 'Food',
-      amount: 1000,
-      date: new Date(2023, 2, 15),
-      LocationOfExpenditure: 'Location:- Restaurant Fifty Five East',
-    },
-    {
-      id: 'e2',
-      expanseItem: 'Expanse Item',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2023, 5, 12),
-      LocationOfExpenditure: 'Location:- Allahabad',
-    },
-    {
-      id: 'e3',
-      expanseItem: 'Expanse Item',
-      title: 'Toilet Paper',
-      amount: 999,
-      date: new Date(2023, 4, 4),
-      LocationOfExpenditure: 'Location:- Bhadohi',
-    },
-    {
-      id: 'e3',
-      expanseItem: 'Expanse Item',
-      title: 'Car Insurance',
-      amount: 5000,
-      date: new Date(2023, 9, 11),
-      LocationOfExpenditure: 'Location:- Suriyawan',
-    },
-    {
-      id: 'e4',
-      expanseItem: 'Expanse Item',
-      title: 'New TV',
-      amount: 4000,
-      date: new Date(2023, 6, 6),
-      LocationOfExpenditure: 'Location:- Varanasi',
-      expanseAmount: 'Expanse of Amount',
-    },
-    {
-      id: 'e5',
-      expanseItem: 'Expanse Item',
-      title: 'Movie',
-      amount: 700,
-      date: new Date(2023, 8, 15),
-      LocationOfExpenditure: 'Location:- Lucknow',
-    },
-    {
-      id: 'e6',
-      expanseItem: 'Expanse Item',
-      title: 'Food',
-      amount: 1000,
-      date: new Date(2023, 7, 7),
-      LocationOfExpenditure: 'Location:- Noida',
-    },
-  ];
+  const [expanses, setExpenses] = useState(DUMMY_EXPENSES);
+  
 
   const addExpenseHandler = expense =>{
-    console.log('In App.js');
-    console.log(expense);
-  }
+  setExpenses((prevExpnses) =>{
+    return [expense, ...prevExpnses]
+  });
+  };
 
   // return React.createElement(
   //   'div',
